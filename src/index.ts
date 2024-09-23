0aae301076480ea75f913cc986ba185112a21e43
Mizan Rahman <113473772+Mizan95@users.noreply.github.com>
2024-09-23 17:48:38 +0100

    Installed dependencies, Initialised calendar and added events

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,28 @@
-import { greetUser } from '$utils/greet';
+import { Calendar } from '@fullcalendar/core';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import listPlugin from '@fullcalendar/list';
+import timeGridPlugin from '@fullcalendar/timegrid';
 
 window.Webflow ||= [];
 window.Webflow.push(() => {
-  const name = 'John Doe';
-  greetUser(name);
+  const calendarElement = document.querySelector<HTMLDivElement>('[data-element="calendar"]');
+  if (!calendarElement) return;
+
+  const calendar = new Calendar(calendarElement, {
+    plugins: [dayGridPlugin, timeGridPlugin, listPlugin],
+    initialView: 'dayGridMonth',
+    headerToolbar: {
+      left: 'prev,next today',
+      center: 'title',
+      right: 'dayGridMonth,timeGridWeek,listWeek',
+    },
+    events: [
+      {
+        title: 'Shaikh Hassan Ali Event',
+        date: '2024-09-08',
+      },
+    ],
+  });
+
+  calendar.render();
 });
